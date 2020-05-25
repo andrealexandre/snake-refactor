1834f05ce1d738938ec770b38551cc27c9ebec0e
Andre Alexandre <andre.alexandre@zalando.ie>
2020-05-25 19:17:30 +0100

    split constructor code from init code. use java 8 lambdas for simplicity of code

--- src/main/java/org/snake/App.java
+++ src/main/java/org/snake/App.java
@@ -1,6 +1,7 @@
 package org.snake;
 
 import org.snake.game.GameMenu;
+import org.snake.settings.GameConfiguration;
 
 public class App {
 
@@ -9,7 +10,10 @@ public class App {
     }
 
     public static void main(String[] args) {
-        new GameMenu();
+        String menuTitle = "Snake Game Menu";
+        final GameConfiguration gameConfiguration = new GameConfiguration();
+        new GameMenu(menuTitle, gameConfiguration).start();
+
         System.out.println(new App().getGreeting());
     }
 }
--- src/main/java/org/snake/game/GameMenu.java
+++ src/main/java/org/snake/game/GameMenu.java
@@ -10,52 +10,43 @@ import javax.swing.JFrame;
 import org.snake.settings.GameConfiguration;
 
 
-public class GameMenu extends JFrame implements ActionListener{
+public class GameMenu extends JFrame {
 	public static final long serialVersionUID = 1L;
 	
 	public GameConfiguration config;
 	
-	public GameMenu(){
-		super("Snake Game Menu");
-		
-		config = new GameConfiguration();
-		
+	public GameMenu(String menuTitle, GameConfiguration config){
+		super(menuTitle);
+		this.config = config;
+	}
+
+	public void start() {
 		setLayout(new BorderLayout());
-		
+
 		JButton start = new JButton("Start");
 		JButton options = new JButton("Options");
 		JButton exit = new JButton("Exit");
-		
-		start.addActionListener(this);
-		options.addActionListener(this);
-		exit.addActionListener(this);
-		
+
+		start.addActionListener(event -> {
+			this.setVisible(false);
+			new GameWindow(this, config);
+		});
+		options.addActionListener(event -> {
+			new OptionsFrame(this);
+		});
+		exit.addActionListener(event -> {
+			System.exit(0);
+		});
+
 		add(start, BorderLayout.NORTH);
 		add(options, BorderLayout.CENTER);
-		add(exit, BorderLayout.SOUTH);		
-				
+		add(exit, BorderLayout.SOUTH);
+
 		setSize(250, 110);
 		setResizable(false);
-		setVisible(true);
 		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);		
-	}
-		
-	public void actionPerformed(ActionEvent a){
-		String s = a.getActionCommand();
-		
-		if(s.equals("Start")){
-			this.setVisible(false);			
-			new GameWindow(this, config);
-		}else{
-			if(s.equals("Options")){				
-				new OptionsFrame(this);				
-			}else{
-				if(s.equals("Exit")){
-					System.exit(0);
-				}
-			}
-		}
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 
 }
